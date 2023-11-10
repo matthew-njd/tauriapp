befd31e69231b16cf2028d4a72a47bd5feeef8a4
Matthew D'Agostino <mnjdagostino@gmail.com>
2023-11-10 09:37:34 -0500

    Prisma init

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,18 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const allCBM = await prisma.CBM_Packing_Information.findMany();
+  console.log(allCBM);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
